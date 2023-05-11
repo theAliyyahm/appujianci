199d59d34f7a99b682825f27f9a872f69e89fbd1
theAliyyahm <maftukhatulaliyyah@gmail.com>
2023-05-11 15:05:24 +0700

    update

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 
 defined('BASEPATH') or exit('No direct script access allowed');
 
@@ -23,14 +24,39 @@ class Welcome extends CI_Controller
      */
     public function index()
     {
+        $this->load->helper('url');
+        if (isset($_POST['nama']) && isset($_POST['nim']) && isset($_POST['umur'])) {
+            $_SESSION['nama'] = $_POST['nama'];
+            $_SESSION['nim'] = $_POST['nim'];
+            $_SESSION['umur'] = $_POST['umur'];
+            redirect('Welcome/tampil');
+        }
+
         $blade = new Blade(VIEWPATH, APPPATH . 'cache');
         echo $blade->make('form', [])->render();
     }
 
     public function tampil()
+    
     {
-        $nama = 'tata';
+        $nama = $_SESSION['nama'];
+        $nim = $_SESSION['nim'];
+        $umur = $_SESSION['umur'];
+        $status = '';
+
+        if ($umur >= 0 && $umur <= 10) {
+            $status = 'Anak';
+        } elseif ($umur > 10 && $umur <= 20) {
+            $status = 'Remaja';
+        } elseif ($umur > 20 && $umur <= 30) {
+            $status = 'Dewasa';
+        } elseif ($umur > 30) {
+            $status = 'Tua';
+        }
+
+        session_unset();
+        session_destroy();
         $blade = new Blade(VIEWPATH, APPPATH . 'cache');
-        echo $blade->make('tampil', ['nama' => $nama])->render();
+        echo $blade->make('tampil', ['nama' => $nama, 'nim' => $nim, 'umur' => $umur, 'status' => $status])->render();
     }
 }
